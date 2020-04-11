19f3159a4b5904eb458fa375d741577d1aea46fd
Ed <BigMac@a-61-4.apts.andrews.edu>
2020-04-11 16:51:55 -0400

    7 segment display
    Created clock page and added a 7 segment display clock

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // Dependencies
 const express = require('express');
-//const johnny_five=require("johnny-five"); 
+// const johnny_five=require("johnny-five");
 
 // Global variables
-app = express();
-//var arduino_board=new johnny_five.Board(); 
-var led_pin=8;
+const app = express();
+// var arduino_board=new johnny_five.Board();
+// var led_pin=8;
 
 // Serves files in the docs directory
 app.listen(3000, () => console.log('listening at http://localhost:3000/'));
-app.use(express.static('docs', {extensions: ['html', 'htm']}));
+app.use(express.static('docs', { extensions: ['html', 'htm'] }));
