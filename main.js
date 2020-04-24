32ae8e9e8d5657481a96e12b3a8ce92544636085
CrypticSquirrel <brejae@andrews.edu>
2020-04-24 16:28:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,31 +5,13 @@
 
 /* ------------------------------ Dependencies ------------------------------ */
 const express = require('express');
-const five = require('johnny-five');
+const { Board, Led } = require('johnny-five');
 const bodyParser = require('body-parser');
 
 /* ---------------------------- Global Variables ---------------------------- */
 const app = express();
-const board = new five.Board();
-// CHANGES: 11 -> 16 12 -> 14 13 -> 15
-// Pins represent: A B C D E F G
-const segmentLEDs = new five.Leds([2, 3, 4, 5, 14, 15, 21]);
-// Left to Right
-const digitPins = new five.Leds([7, 8, 10, 16]);
-
-// Each LED group represents a number 0-9
-const SevenSegNumbers = [
-    new five.Leds([2, 3, 4, 5, 14]),
-    new five.Leds([3, 4]),
-    new five.Leds([2, 3, 5, 21]),
-    new five.Leds([2, 3, 4, 5, 21]),
-    new five.Leds([3, 4, 15, 21]),
-    new five.Leds([2, 4, 5, 15, 21]),
-    new five.Leds([2, 4, 5, 14, 15, 21]),
-    new five.Leds([2, 3, 4]),
-    new five.Leds([2, 3, 4, 5, 14, 15, 21]),
-    new five.Leds([2, 3, 4, 5, 15, 21]),
-];
+const board = new Board();
+let digits;
 
 /* ----------------------------- Initialization ----------------------------- */
 
@@ -39,68 +21,49 @@ app.use(bodyParser.json());
 app.listen(3000, () => console.log('listening at http://localhost:3000/'));
 app.use(express.static('docs'));
 
-/* ---------------------------- Helper Functions ---------------------------- */
+/* ---------------------------- Routing ---------------------------- */
 
-const resetDisplay = () => {
-    digitPins.forEach(item => {
-        item.on();
-        segmentLEDs.off();
-        item.off();
-    });
-};
-
-/* ----------------------------- Arduino Routing ---------------------------- */
-
-board.on('ready', function() {
-    /**
-     * Route that gets the time from the client-side javascript to the 7-segment display.
-     */
-    app.post('/time', (req, res) => {
-        let timeData;
-        if (req.body.isHoursDisplayed === 'true') {
-            timeData = [
-                parseInt(req.body.hours[0]),
-                parseInt(req.body.hours[1]),
-                parseInt(req.body.minutes[0]),
-                parseInt(req.body.minutes[1]),
-            ];
-        } else {
-            timeData = [
-                parseInt(req.body.minutes[0]),
-                parseInt(req.body.minutes[1]),
-                parseInt(req.body.seconds[0]),
-                parseInt(req.body.seconds[1]),
-            ];
-        }
+/**
+ * Route that gets the time from the client-side javascript to the 7-segment display.
+ */
+app.post('/time', (req, res) => {
+    console.log(`${req.body.minutes}:${req.body.seconds}`);
+    if (req.body.isHoursDisplayed === 'true') {
         if (req.body.isRegularFormat === 'true') {
-            timeData.forEach((number, index) => {
-                timeData[index] = number % 12;
-            });
+            const regHours = parseInt(req.body.hours) % 12;
+            digits.print(`${regHours}:${req.body.minutes}`);
+        } else {
+            digits.print(`${req.body.hours}:${req.body.minutes}`);
         }
-
-        resetDisplay();
-        timeData.forEach((number, index) => {
-            digitPins[index].on();
-            SevenSegNumbers[number].on();
-            digitPins[index].off();
-        });
-    });
-
-    /**
-     * Route to reset 7-segment display.
-     */
-    app.post('/reset', () => {
-        resetDisplay();
-    });
-
-    /**
-     * Route to control each segment in the 7-segment display.
-     */
-    app.post('/sandbox', (req, res) => {
-        const pinNumber = parseInt(req.body.number);
-        const segment = parseInt(req.body.segment);
-        digitPins[pinNumber].on();
-        segmentLEDs[segment].toggle();
-        digitPins[pinNumber].off();
-    });
+    } else {
+        digits.print(`${req.body.minutes}:${req.body.seconds}`);
+    }
 });
+
+/**
+ * Route to reset 7-segment display.
+ */
+app.post('/reset', () => {
+    digits.clear();
+});
+
+/**
+ * Route to control each segment in the 7-segment display.
+ */
+app.post('/sandbox', (req, res) => {
+    const pinNumber = parseInt(req.body.number);
+    const segment = parseInt(req.body.segment);
+});
+
+/* ----------------------------- Arduino Initialization ---------------------------- */
+
+function startServer() {
+    app.listen('5000', () => {
+        console.log('App listening on port 5000');
+    });
+    digits = new Led.Digits({
+        controller: 'HT16K33',
+    });
+}
+
+board.on('ready', startServer);
